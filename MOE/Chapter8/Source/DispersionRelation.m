f1a5c248ab02069eabb032ce72ab9cf0d3955fa9
NoMoonAndStar <3540629275@qq.com>
2024-07-21 14:55:06 +0800

    绘制了色散曲线

--- MOE/Chapter8/Source/DispersionRelation.m
+++ MOE/Chapter8/Source/DispersionRelation.m
@@ -1,17 +1,28 @@
 d = 0.01;
 syms betad
-kd = 0:0.001:1;
-k = kd / d;
-sd = zeros(length(kd), 2);
+kd = 0:0.01:8;
+realbetad = zeros(1, length(kd));
 
 for i = 1:length(kd)
     f = cos(betad) - cos(kd(i)) + 2 * kd(i) * sin(kd(i));
     s = solve(f == 0);
-    sd(i, :) = double(s);
+
+    if isreal(s)
+
+        if sum(double(s) >= 0)
+            realbetad(i) = min(double(s(double(s) >= 0)));
+        end
+
+    end
+
 end
 
-beta = sd / d;
-[rows, cols] = find(imag(beta) == 0);
-realbeta = real(beta(rows, cols));
+%剔除虚数解，除第一个0外都删除
+realbetad2 = realbetad(2:end);
+kd2 = kd(2:end);
+idxreal = realbetad2 > 0;
+realbetad = [0, realbetad2(idxreal)];
+kd = [0, kd2(idxreal)];
 
-plot(k, realbeta', '.');
+%绘制色散图
+plot(realbetad, kd, '.');
