fe18217b01596e7a45e8e0f6d22914dd0ce69919
NoMoonAndStar <3540629275@qq.com>
2024-07-24 15:01:56 +0800

    提交了4.5b的点图

--- CRLH/Source/BalancedCRLH2D.m
+++ CRLH/Source/BalancedCRLH2D.m
@@ -13,7 +13,7 @@ CL = 1e-12; %右手单位长度电感
 
 %扫频范围
 Omega = 1e11;
-omega = 0:Omega / 100000:Omega;
+omega = 0:Omega / 20:Omega;
 
 %晶格间距
 px = 10 * 1e-3;
@@ -24,6 +24,7 @@ Z = 1i * omega * LR / 2 + 1 ./ (1i * 2 * omega * CL);
 Y = 1i * omega * CR + 1 ./ (1i * omega * LL);
 x = -Z .* Y;
 
+%% fig4.5(a)
 %gamma->X
 kpx = acos(1 - x / 2);
 kpy = 0;
@@ -79,3 +80,57 @@ ylim([0 Omega])
 xlabel('Beta*p')
 ylabel('omega')
 title('Dispersion relation from M to gamma')
+
+%% fig4.5(b)
+kpx = linspace(-pi, pi, 100);
+kpy = linspace(-pi, pi, 100);
+
+% 创建网格
+[KPX, KPY] = meshgrid(kpx, kpy);
+Z = cos(KPX) + cos(KPY);
+k = 2 - x / 2;
+%取实数解
+idx = k >= -2 & k <= 2;
+k = k(idx);
+omega4 = omega(idx);
+eps = 1e-3;
+results_kpx = cell(1, length(k));
+results_kpy = cell(1, length(k));
+
+for i = 1:length(k)
+    xy_pairs = find(abs(Z - k(i)) < eps);
+    [x_sol, y_sol] = ind2sub(size(Z), xy_pairs);
+    results_kpx{i} = kpx(x_sol);
+    results_kpy{i} = kpy(y_sol);
+end
+
+max_length = max(cellfun(@numel, results_kpx));
+
+% 创建一个大数组并用NaN填充
+big_array_kpx = NaN(length(k), max_length);
+big_array_kpy = NaN(length(k), max_length);
+
+% 填充数组
+for i = 1:length(k)
+    len = numel(results_kpx{i});
+    big_array_kpx(i, 1:len) = results_kpx{i};
+    big_array_kpy(i, 1:len) = results_kpy{i};
+    %现在big_array每一行对应一个频率的kpx和kpy的值
+end
+
+% 将数组转换为列向量
+X = big_array_kpx';
+Y = big_array_kpy';
+X = X(:)';
+Y = Y(:)';
+% 将频率转换为对角矩阵
+Z = Remap(omega4, max_length);
+Z(Z == 0) = NaN;
+
+h2 = figure;
+plot3(X, Y, Z, 'b.')
+xlabel('kx*px')
+ylabel('ky*py')
+zlabel('omega')
+title('Dispersion relation of 2D CRLH')
+grid on
