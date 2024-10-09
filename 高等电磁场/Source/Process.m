919ba19dd19e5a92f65f39223bb5f35633d67db8
NoMoonAndStar <3540629275@qq.com>
2024-10-09 15:03:47 +0800

    高等电磁场

--- 高等电磁场/Source/Process.m
+++ 高等电磁场/Source/Process.m
@@ -2,93 +2,167 @@ clear
 clc
 close all
 tic
-%% 导入CST中的电场和磁场（时谐场）
-% 加载电场数据
-Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\e-field (f=1) [pw].csv", ';', 2).data;
-% 加载磁场数据
-Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\h-field (f=1) [pw].csv", ';', 2).data;
-% 获取数据长度
-N = length(Ed(:, 1));
-
-% 初始化结构体存储点的信息
-points = struct('x', [], 'y', [], 'z', [], 'Ex', [], 'Ey', [], 'Ez', [], 'Hx', [], 'Hy', [], 'Hz', [], 'Sx', [], 'Sy', [], 'Sz', []);
-points = repmat(points, N, 1);
-
-% 遍历所有数据点
-for i = 1:N
-    points(i).x = Ed(i, 1) * 1e-3; % x坐标
-    points(i).y = Ed(i, 2) * 1e-3; % y坐标
-    points(i).z = Ed(i, 3) * 1e-3; % z坐标
-    points(i).Ex = complex(Ed(i, 4), Ed(i, 5)); % 复数形式的Ex
-    points(i).Ey = complex(Ed(i, 6), Ed(i, 7)); % 复数形式的Ey
-    points(i).Ez = complex(Ed(i, 8), Ed(i, 9)); % 复数形式的Ez
-    points(i).Hx = complex(Hd(i, 4), Hd(i, 5)); % 复数形式的Hx
-    points(i).Hy = complex(Hd(i, 6), Hd(i, 7)); % 复数形式的Hy
-    points(i).Hz = complex(Hd(i, 8), Hd(i, 9)); % 复数形式的Hz
-    points(i).Sx = 1/2 * (points(i).Ey * conj(points(i).Hz) - points(i).Ez * conj(points(i).Hy)); % 复坡印廷矢量
-    points(i).Sy = 1/2 * (points(i).Ez * conj(points(i).Hx) - points(i).Ex * conj(points(i).Hz));
-    points(i).Sz = 1/2 * (points(i).Ex * conj(points(i).Hy) - points(i).Ey * conj(points(i).Hx));
-end
-
-% 设置介质参数
+%% 设置介质参数
 epsr = 5; % 相对介电常数=' .
 mur = 1; % 相对磁导率
 eps0 = 8.854e-12; % 真空介电常数
 mu0 = 4 * pi * 1e-7; % 真空磁导率
-tanD = 0; % 损耗角正切
-epsi = tanD * eps0 * epsr;
+tanD = 0.1; % 损耗角正切
+epsi = tanD * epsr;
 mui = 0;
 f = 1 * 1e9; % 频率
 dx = 1 * 1e-3; % 积分区域尺寸
 dy = 10 * 1e-3;
 dz = 1 * 1e-3;
 
-%% 求右边
-% 储能
-w_store = zeros(N, 1);
+%% 计算xm面上的面积分
+% 加载电场数据
+Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\Exm_e-field (f=1) [pw].csv", ';', 2).data;
+% 加载磁场数据
+Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\Hxm_h-field (f=1) [pw].csv", ';', 2).data;
+% 获取数据长度
+N = length(Ed(:, 1));
 
-for i = 1:N
-    w_store(i) = 1/2 * 2 * pi * f * ((mu0 * mur * sum([points(i).Hx, points(i).Hy, points(i).Hz] .* conj([points(i).Hx, points(i).Hy, points(i).Hz]))) - eps0 * epsr * sum([points(i).Ex, points(i).Ey, points(i).Ez] .* conj([points(i).Ex, points(i).Ey, points(i).Ez])));
-end
+Ex = complex(Ed(:, 4), Ed(:, 5));
+Ey = complex(Ed(:, 6), Ed(:, 7));
+Ez = complex(Ed(:, 8), Ed(:, 9));
+Hx = complex(Hd(:, 4), Hd(:, 5));
+Hy = complex(Hd(:, 6), Hd(:, 7));
+Hz = complex(Hd(:, 8), Hd(:, 9));
+Sx = 1/2 * (Ey .* conj(Hz) - conj(Hy) .* Ez);
+Sy = 1/2 * (Ez .* conj(Hx) - conj(Hz) .* Ex);
+Sz = 1/2 * (Ex .* conj(Hy) - conj(Hx) .* Ex);
 
-iright = 1i * mean(w_store) * dx * dy * dz;
+Sint_xm = -mean(Sx) * dy * dz;
 
-% 损耗
-w_loss = zeros(N, 1);
+%% 计算xm面上的面积分
+% 加载电场数据
+Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\Exp_e-field (f=1) [pw].csv", ';', 2).data;
+% 加载磁场数据
+Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\Hxp_h-field (f=1) [pw].csv", ';', 2).data;
+% 获取数据长度
+N = length(Ed(:, 1));
 
-for i = 1:N
-    w_loss(i) = 1/2 * 2 * pi * f * ((mui * sum([points(i).Hx, points(i).Hy, points(i).Hz] .* conj([points(i).Hx, points(i).Hy, points(i).Hz]))) + epsi * sum([points(i).Ex, points(i).Ey, points(i).Ez] .* conj([points(i).Ex, points(i).Ey, points(i).Ez])));
-end
+Ex = complex(Ed(:, 4), Ed(:, 5));
+Ey = complex(Ed(:, 6), Ed(:, 7));
+Ez = complex(Ed(:, 8), Ed(:, 9));
+Hx = complex(Hd(:, 4), Hd(:, 5));
+Hy = complex(Hd(:, 6), Hd(:, 7));
+Hz = complex(Hd(:, 8), Hd(:, 9));
+Sx = 1/2 * (Ey .* conj(Hz) - conj(Hy) .* Ez);
+Sy = 1/2 * (Ez .* conj(Hx) - conj(Hz) .* Ex);
+Sz = 1/2 * (Ex .* conj(Hy) - conj(Hx) .* Ex);
 
-rright = mean(w_loss) * dx * dy * dz;
-right = iright + rright;
+Sint_xp = mean(Sx) * dy * dz;
 
-%% 求左边：坡印廷矢量的面积分
+%% 计算ym面上的面积分
+% 加载电场数据
+Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\Eym_e-field (f=1) [pw].csv", ';', 2).data;
+% 加载磁场数据
+Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\Hym_h-field (f=1) [pw].csv", ';', 2).data;
+% 获取数据长度
+N = length(Ed(:, 1));
 
-Sint_x = [];
-Sint_y = [];
-Sint_z = [];
+Ex = complex(Ed(:, 4), Ed(:, 5));
+Ey = complex(Ed(:, 6), Ed(:, 7));
+Ez = complex(Ed(:, 8), Ed(:, 9));
+Hx = complex(Hd(:, 4), Hd(:, 5));
+Hy = complex(Hd(:, 6), Hd(:, 7));
+Hz = complex(Hd(:, 8), Hd(:, 9));
+Sx = 1/2 * (Ey .* conj(Hz) - conj(Hy) .* Ez);
+Sy = 1/2 * (Ez .* conj(Hx) - conj(Hz) .* Ex);
+Sz = 1/2 * (Ex .* conj(Hy) - conj(Hx) .* Ex);
 
-for i = 1:N
+Sint_ym = -mean(Sy) * dx * dz;
 
-    if points(i).y == 0
-        Sint_y = [Sint_y, -points(i).Sy];
-    elseif points(i).y == dy
-        Sint_y = [Sint_y, points(i).Sy];
-    elseif points(i).x == -dx / 2
-        Sint_x = [Sint_x, -points(i).Sx];
-    elseif points(i).x == dx / 2
-        Sint_x = [Sint_x, points(i).Sx];
-    elseif points(i).z == 0
-        Sint_z = [Sint_z, -points(i).Sz];
-    elseif points(i).z == dz
-        Sint_z = [Sint_z, points(i).Sz];
-    end
+%% 计算yp面上的面积分
+% 加载电场数据
+Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\Eyp_e-field (f=1) [pw].csv", ';', 2).data;
+% 加载磁场数据
+Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\Hyp_h-field (f=1) [pw].csv", ';', 2).data;
+% 获取数据长度
+N = length(Ed(:, 1));
 
-end
+Ex = complex(Ed(:, 4), Ed(:, 5));
+Ey = complex(Ed(:, 6), Ed(:, 7));
+Ez = complex(Ed(:, 8), Ed(:, 9));
+Hx = complex(Hd(:, 4), Hd(:, 5));
+Hy = complex(Hd(:, 6), Hd(:, 7));
+Hz = complex(Hd(:, 8), Hd(:, 9));
+Sx = 1/2 * (Ey .* conj(Hz) - conj(Hy) .* Ez);
+Sy = 1/2 * (Ez .* conj(Hx) - conj(Hz) .* Ex);
+Sz = 1/2 * (Ex .* conj(Hy) - conj(Hx) .* Ex);
 
-left =- 2 * (mean(Sint_x) * dy * dz + mean(Sint_y) * dz * dx + mean(Sint_z) * dx * dy);
+Sint_yp = mean(Sy) * dx * dz;
 
-%% 对比结果
+%% 计算zm面上的面积分
+% 加载电场数据
+Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\Ezm_e-field (f=1) [pw].csv", ';', 2).data;
+% 加载磁场数据
+Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\Hzm_h-field (f=1) [pw].csv", ';', 2).data;
+% 获取数据长度
+N = length(Ed(:, 1));
+
+Ex = complex(Ed(:, 4), Ed(:, 5));
+Ey = complex(Ed(:, 6), Ed(:, 7));
+Ez = complex(Ed(:, 8), Ed(:, 9));
+Hx = complex(Hd(:, 4), Hd(:, 5));
+Hy = complex(Hd(:, 6), Hd(:, 7));
+Hz = complex(Hd(:, 8), Hd(:, 9));
+Sx = 1/2 * (Ey .* conj(Hz) - conj(Hy) .* Ez);
+Sy = 1/2 * (Ez .* conj(Hx) - conj(Hz) .* Ex);
+Sz = 1/2 * (Ex .* conj(Hy) - conj(Hx) .* Ex);
+
+Sint_zm = -mean(Sz) * dx * dy;
+
+%% 计算zp面上的面积分
+% 加载电场数据
+Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\Ezp_e-field (f=1) [pw].csv", ';', 2).data;
+% 加载磁场数据
+Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\Hzp_h-field (f=1) [pw].csv", ';', 2).data;
+% 获取数据长度
+N = length(Ed(:, 1));
+
+Ex = complex(Ed(:, 4), Ed(:, 5));
+Ey = complex(Ed(:, 6), Ed(:, 7));
+Ez = complex(Ed(:, 8), Ed(:, 9));
+Hx = complex(Hd(:, 4), Hd(:, 5));
+Hy = complex(Hd(:, 6), Hd(:, 7));
+Hz = complex(Hd(:, 8), Hd(:, 9));
+Sx = 1/2 * (Ey .* conj(Hz) - conj(Hy) .* Ez);
+Sy = 1/2 * (Ez .* conj(Hx) - conj(Hz) .* Ex);
+Sz = 1/2 * (Ex .* conj(Hy) - conj(Hx) .* Ex);
+
+Sint_zp = mean(Sz) * dx * dy;
+
+%% 计算长方体内的体积分
+% 加载电场数据
+Ed = importdata("D:\CST\S-TheroyTEM\Export\3d\Ev_e-field (f=1) [pw].csv", ';', 2).data;
+% 加载磁场数据
+Hd = importdata("D:\CST\S-TheroyTEM\Export\3d\Hv_h-field (f=1) [pw].csv", ';', 2).data;
+% 获取数据长度
+N = length(Ed(:, 1));
+
+Ex = complex(Ed(:, 4), Ed(:, 5));
+Ey = complex(Ed(:, 6), Ed(:, 7));
+Ez = complex(Ed(:, 8), Ed(:, 9));
+Hx = complex(Hd(:, 4), Hd(:, 5));
+Hy = complex(Hd(:, 6), Hd(:, 7));
+Hz = complex(Hd(:, 8), Hd(:, 9));
+wes = 1/4 * epsr * eps0 * sum([Ex, Ey, Ez] .* conj([Ex, Ey, Ez]), 2);
+wms = 1/4 * mur * mu0 * sum([Hx, Hy, Hz] .* conj([Hx, Hy, Hz]), 2);
+wel = 1/2 * epsi * eps0 * sum([Ex, Ey, Ez] .* conj([Ex, Ey, Ez]), 2);
+wml = 1/2 * mui * mu0 * sum([Hx, Hy, Hz] .* conj([Hx, Hy, Hz]), 2);
+
+We = 2 * 2 * pi * f * mean(wes) * dx * dy * dz;
+Wh = 2 * 2 * pi * f * mean(wms) * dx * dy * dz;
+Wstore = 2 * 2 * pi * f * mean(wms - wes) * dx * dy * dz;
+Wloss = 2 * pi * f * mean(wel + wml) * dx * dy * dz;
+
+%% 整合结果
+left =- (Sint_xm + Sint_xp + Sint_ym + Sint_yp + Sint_zm + Sint_zp);
+right = complex(Wloss, Wstore);
+
+table(We,Wh)
 table(left, right)
-toc
+
